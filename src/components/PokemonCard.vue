56b45513d82a60ca8123e7ec2b8158681ef60812
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-06-20 19:39:24 +0200

    added fetch

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,39 +1,81 @@
 <script setup>
+    import { pokeapi } from '@/services/pokeapi.js';
     defineProps({
-        cardId: {
-            type: Number
-        },
-        cardClass: {
-            type: String
-        },
-        cardName: {
-            type: String
-        },
+        // cardId: {
+        //     type: String
+        // },
+        // cardClass: {
+        //     type: String
+        // },
+        // cardName: {
+        //     type: String
+        // },
         imgUrl: {
             type: String
         },
-        typeA: {
-            type: String
-        },
-        typeB: {
-            type: String,
-            default: ""
-        }
-    })
+        // typeA: {
+        //     type: String
+        // },
+        // typeB: {
+        //     type: String,
+        //     default: ""
+        // },
+        // data(){
+        //    return {
+        //     pokemonData: {},
+        //     pokemonID: ''
+        //    } 
+        // },
+
+            
+        
+    });
+    async function searchPokemon() {
+                    try {
+                        const pokemonToFind = await fetch(`${pokeapi}/${this.pokemonID}`);
+                        const pokemon = await pokemonToFind.json();
+                        this.pokemonData = pokemon;
+                        console.log(pokemon);
+                        return pokemon;
+                    } catch (error) {
+                        alert('Pokemon was not found')
+                    }
+                }
+    
 </script>
 
 
 <template>
     <div :class="cardClass">
-        <h3> {{ cardId }} </h3>
-        <h2>{{ cardName }}</h2>
+        <h3> {{ pokemonID }} </h3>
+        <h2>{{ pokemonName }}</h2>
         <img :src="imgUrl">
         <p>{{ typeA }}</p>
         <p>{{ typeB }}</p>
     </div>
+
+    <div>
+        <input type="text" v-model="pokemonID">
+        <button @click="searchPokemon()">Search Pokemon</button>
+    </div>
+
 </template>
 
 
 <style scoped>
+    div {
+        background: rgb(109, 102, 102);
+        width: 70%;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    div img {
+        width: 250px;
+    }
     
+    .fuego{
+        background-color: red;
+    }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 import PokemonCard from '../components/PokemonCard.vue';
 </script>
 
 <template>
   <main>
-    <PokemonCard cardId="0001" cardName="Charmander" imgUrl="src/assets/charmander.png" typeA="Fuego"/>
+    <PokemonCard />
   </main>
 </template>
