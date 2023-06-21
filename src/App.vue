1caf3c8424ff47d72215b61a6345da841f664bda
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-06-21 19:55:54 +0200

    Emits from child to father

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+//import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
 import SearchView from './views/SearchView.vue';
+import { ref } from 'vue'
+
+const objectFromChild=ref();
+
+function RecuperaDelHijo(data){
+  objectFromChild.value=data.id;
+}
+
 </script>
 
 <template>
 
-  <SearchView></SearchView>
+  <SearchView @send-datos="RecuperaDelHijo"></SearchView>  
+
+  <p>{{objectFromChild}}</p>
 
   <!--<header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
--- src/services/ConnectApi.js
+++ src/services/ConnectApi.js
@@ -4,13 +4,13 @@ export default async function ConnectApi(onePokemon){
             const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+onePokemon);
             const data = await response.json();
 
-            console.log(data);
+            //console.log(data);
 
-            return "2";
+            return data;
         }
         catch{
             console.log("That Pokemon Doesn't exist! (yet)");
-            return "1"
+            return 404;
         }
     
 }
