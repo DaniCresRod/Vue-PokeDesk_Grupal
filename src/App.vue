0f799a88644d02f6d018c8bc795f00d8c3344d12
carmenb2020 <carmenborjap@hotmail.com>
2023-06-22 11:10:22 +0200

    recovery app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import RandomView from './components/RandomView.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import SearchView from './views/SearchView.vue';
+import PokemonCard from './components/PokemonCard.vue';
 </script>
 
 <template>
-  <header>
+
+  <SearchView></SearchView>
+  <PokemonCard cardClass="fire" imgUrl="src/assets/charmander.png"/>
+  <!--<header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
       <nav>
-      
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
+    </div>
   </header>
 
+  <RouterView />-->
 </template>
 
 <style scoped>
-ter-link-exact-active:hover {
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
 
